7ec7973740276fab3dac4c259aa4e057c9009d9f
Eve <95377553+fishyEve@users.noreply.github.com>
2024-01-01 15:15:40 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Hi</title>
+	<link rel="stylesheet" type="text/css" href="style.css"> <!-- type is function (other css file) to predefined text stuff same w href -->
+</head>
+<body>
+	</body>
+</html>
