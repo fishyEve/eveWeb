24bdd0b96b9fbcb6c7c9829d74a3dcba752ced0c
Eve <95377553+fishyEve@users.noreply.github.com>
2024-06-01 10:30:32 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,89 @@
 <!DOCTYPE html>
+<div class="HomeBody">
 <html lang="en">
 <head>
-	<title>Hi</title>
+	<title>EveWeb</title>
 	<link rel="stylesheet" type="text/css" href="style.css"> <!-- type is function (other css file) to predefined text stuff same w href -->
 </head>
+
 <body>
-	</body>
+	<h1>Hi there, traveler!</h1>
+
+	
+	<h2>My name is Eve, this is my website</h2>
+	
+
+	<p>Since you're on my website, maybe you should get to know me.</p>
+
+	<!-- This is an unordered list-->
+	<p>I like:</p>
+	<ul>
+		<li>movies</li>
+		<li>gyros and curry (but not together)</li>
+		<li>the colors purple and green</li>
+		<li>espresso</li>
+		<li>Nintendo games</li>
+		<li>baking</li>
+	</ul>
+
+	<p>I don't like:</p>
+        <ul>
+                <li>coding with assembly</li>
+                <li>bugs (both the insect and the coding term)</li>
+                <li>lasagna</li>
+                <li>wet socks</li>
+                <li>Homestuck (ok that's kind of a lie, it went hard in 2016)</li>
+                <li>movies where the protagonist realizes everything was a dream at the end</li>
+        </ul>
+	<p>And this is what I look like:</p>
+	<img class='centerimages' border="4px" src="images/stickFigure.png" alt="image" stle='width:50px;height:150px;'>
+
+	<!-- This is an ordered list -->
+
+	<!-- <ol>
+		<li>Tim</li>
+		<li>Eve</li>
+		<li>Eve2</li>
+		<li>Eve3</li>
+		<li>Sofie</li>
+	</ol> -->
+
+	<br /> <!-- These are spaces -->
+        <br />
+
+	<!-- This is a table -->
+
+	<!--<table>
+		<tr>
+			<td>Col 1</td>
+			<td>Col 2</td>
+			<td>Col 3</td>
+		</tr>
+		<tr>
+			<td>X</td>
+			<td>Y</td>
+			<td>Z</td>
+		</tr>
+		<tr>
+			<td>X</td>
+			<td>Y</td>
+			<td>Z</td>
+		</tr>
+		<tr>
+			<td>X</td>
+			<td>Y</td>
+			<td>Z</td>
+		</tr>
+
+	</table>-->
+	<p> now that we're acquainted, welcome to my website! You can use the menu below to navigate around</p>
+	<?php include 'nav.html'?>
+	<br />
+	<br />
+	<br />
+	<br />
+	<img class='centerimages' border="0px" src="images/giphy.gif" alt="image" stle='width:250px;height:300px;'>
+	<h2>Last Update: January 2024</h2>
+</body>
 </html>
+</div>
